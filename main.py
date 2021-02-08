30b7ee2cc720a4a51df90ce749e60a08599230e9
Mark Miller <markjmiller17@gmail.com>
2021-02-08 18:35:24 -0500

    format something

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import requests
 import secrets
 
 
-def get_data(url:str):
+def get_data():
     all_data = []
-    api_url = f"{url}&api_key={secrets.api_key}"
-    
+    for page in range(162):
+        response = requests.get(f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&page={page}")
+        if response.status_code != 200:
+            print("error getting data")
+
+        page_of_data = response.json()
+        page_of_school_data = page_of_data['results']
+        all_data.extend(page_of_school_data)
+    return all_data
+
+
+def main():
+    college_data = get_data()
+
+if __name__ == '__main__':
+    main()
