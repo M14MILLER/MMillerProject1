994af12321385038b9bd791013d1e72831e12cee
Mark Miller <markjmiller17@gmail.com>
2021-03-01 23:20:07 -0500

    update with prospective new table as well as adding requirements file

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import secrets
 import json
 import sqlite3
 from typing import Tuple
-#import random
+from openpyxl import load_workbook
 
+workbook = load_workbook(filename="state_M2019_dl.xslx")
+sheet = workbook.active
+
+states = {}
 
 def get_data():
     all_data = []
     for page in range(162):
         response = requests.get(f"https://api.data.gov/ed/collegescorecard/v1/schools.json?"
-                                f"school.degrees_awarded.predominant=2,3&fields=school.name,school.city,"
+                                f"school.degrees_awarded.predominant=2,3&fields=id,school.name,school.city,"
                                 f"2018.student.size,2017.student.size,"
                                 f"2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,"
                                 f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&page={page}")
@@ -32,27 +36,52 @@ def close_db(connection: sqlite3.Connection):
     connection.close()
 
 def setup_db(cursor:sqlite3.Cursor):
-    cursor.execute('''CREATE TABLE IF NOT EXISTS college_data(school_name TEXT NOT NULL,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS college_data(
+    school_id INTEGER PRIMARY KEY,
+    school_name TEXT NOT NULL,
     school_city TEXT NOT NULL, 
-    The2018_student_size INTEGER DEFAULT 0,
-    The2017_student_size INTEGER DEFAULT 0,
-    The2017_earnings_3_yrs_after_completion_overall_count_over_poverty_line REAL DEFAULT 0,
-    The2016_repayment_3_yr_repayment_overall REAL DEFAULT 0,
+    student_size INTEGER,
+    three_year_earnings_over_poverty INTEGER,
+    loan_repayment INTEGER
     );''')
 
-def add_college(cursor: sqlite3.Cursor, name, city, T2018size, T2017size, T2017earn, T2016repay):
-    cursor.execute(f'''INSERT INTO COLLEGES (school_name, school_city, The2018_student_size, The2017_student_size, 
-                    The2017_earnings_3_yrs_after_completion_overall_count_over_poverty_line, The2016_repayment_3_yr_repayment_overall)
-    VALUES({name}, {city}, {T2018size}, {T2017size}, {T2017earn}, {T2016repay})''')
+def state_employment(cursor:sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS state_by_state_employment(
+    employment_state TEXT NOT NULL,
+    job_title TEXT NOT NULL,
+    total_employment INTEGER,
+    twenty_fifth_percentile_salary INTEGER,
+    job_code''')
+
+def add_college_table_data(all_data, cursor):
+    for coll_data in all_data:
+        cursor.execute("""
+            INSERT INTO college_data(school_id, school_city, school_name, student_size, three_year_earnings_over_poverty,
+             loan_repayment)
+             VALUES (?,?,?,?,?,?);
+            """, (coll_data['id'], coll_data['school.city'], coll_data['school.name'], coll_data['2018.student.size'],
+                  coll_data['2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line'],
+                  coll_data['2016.repayment.3_yr_repayment.overall']))
+   #for row in sheet.iter_rows():
+     #   if sheet.columns[9].values="":
+        #    cursor.execute("""
+              #      INSERT INTO state_by_state_employment(employment_state, job_title, total_employment,
+                #    twenty_fifth_percentile_salary, job_code)
+                 #    VALUES (?,?,?,?,?,?);""", (
+#                   row['state'], row['title'], row['tot_emp], row[twenty_five_percent'], row['code']
+
 
 def main():
+    all_data = get_data()
     conn, cursor = open_db("college_db.sqlite")
     setup_db(cursor)
-    college_data = get_data()
+    add_college_table_data(all_data, cursor)
     with open('school_data.txt', 'w') as outfile:
-        json.dump(college_data, outfile)
-    print(college_data)
+        json.dump(add_college_table_data(), outfile)
+    print(add_college_table_data())
+    print(json.dumps(states))
     close_db(conn)
+
 
 
 
